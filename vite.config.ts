c22c33a70b9f96c0587495acd3c676b149e2fdb5
BinaryTerror <linoalfredoficher@gmail.com>
2026-08-02 23:35:03 +0200

    dosc

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/valeo-precious-mines/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
 })
